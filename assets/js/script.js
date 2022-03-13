6a37eba17432751a44e4d02df4ee55c47676b016
MadDAWG <[madelinejdierking@gmail.com]>
2022-03-13 17:52:26 -0400

    added calendar

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,7 +74,7 @@ $("#task-form-modal .btn-primary").click(function() {
       date: taskDate
     });
 
-    saveTasks();
+    saveTasks();  
     
   }
 });
@@ -86,25 +86,21 @@ $(".list-group").on("click", "p", function() {
     .text()
     .trim();
 
-  var textImput = $("<textarea>")
-  .addClass("form-control")
-  .val(text);
+  // replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
   $(this).replaceWith(textInput);
+
+  //auto focus new element
   textInput.trigger("focus");
 });
 
 $("list-group").on("blur", "textarea", function () {
-  var text = $(this)
-    .val()
-    .trim();
-
-    //get the parent ul's id attribute
+  var text = $(this).val();
+  
   var status = $(this)
     .closest(".list-group")
     .attr("id")
     .replace("list-", "");
-
-  //get the task's position in the list of other li elements
   var index = $(this)
     .closest(".list-group-item")
     .index();
@@ -119,6 +115,46 @@ $("list-group").on("blur", "textarea", function () {
 
   // replace textarea with p element
   $(this).replaceWith(taskP);
+});
+
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+  //create new input element
+  var dateInput = $("<input>")
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
+  $(this).replaceWith(dateInput);
+
+  //automatically bring up the calendar
+  dateInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span and insert in place of input element
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+    $(this).replaceWith(taskSpan);
 });
 
 // remove all tasks
